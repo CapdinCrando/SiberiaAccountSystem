7350328a3b66fca39e2c7387e85191c6c04f2aa9
Tristan Jay <capdincrando@gmail.com>
2021-05-26 12:02:15 -0500

    Update accountApi.lua

--- accountApi.lua
+++ accountApi.lua
@@ -1,5 +1,6 @@
 local ttf = require("tableToFile")
 local component = require("component")
+local event = require("event")
 local modem = component.modem
 
 local accountApi = {}
@@ -9,22 +10,33 @@ local data = {}
 local address = 0
 local port = 0
 
+local function getModemMessage()
+	local _, _, from, _, _, message = event.pull("modem_message")
+	if from == address then
+		return message
+	end
+end
+
 function accountApi:loadFile()
 	data = ttf.load(serverFile)
 	address = data["address"]
 	port = data["port"]
+	modem.open(port)
 end
 
 function accountApi:createAccount(name)
-	return modem.send(address, port, "createAccount", name)
+	modem.send(address, port, "createAccount", name)
+	return getModemMessage()
 end
 
 function accountApi:getAmount(name)
-	return modem.send(address, port, "getAmount", name)
+	modem.send(address, port, "getAmount", name)
+	return getModemMessage()
 end
 
 function accountApi:transfer(sender, receiver, amount)
-	return modem.send(address, port, "transfer", sender, receiver, amount)
+	modem.send(address, port, "transfer", sender, receiver, amount)
+	return getModemMessage()
 end
 
 return accountApi
